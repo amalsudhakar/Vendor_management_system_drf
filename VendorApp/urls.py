20371de614079e6615f3fa79ecd3c79aad6cdda0
amalsudhakar <amalsudhakar52@gmail.com>
2023-12-09 19:55:17 +0530

    Vendor api

--- VendorApp/urls.py
+++ VendorApp/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import VendorsList, VendorCreate
+from .views import VendorsList, VendorCreate, VendorDetails, VendorUpdate, VendorDelete
 
 urlpatterns = [
     path('vendors/', VendorCreate.as_view(), name='create-vendor'),
     path('vendors/list/', VendorsList.as_view(), name='vendor-list'),
+    path('vendors/<int:vendor_id>/', VendorDetails.as_view(), name='vendor-details'),
+    path('vendors/update/<int:vendor_id>/', VendorUpdate.as_view(), name='vendor-update'),
+    path('vendors/delete/<int:vendor_id>/', VendorDelete.as_view(), name='vendor-dekete'),
 ]
--- VendorApp/views.py
+++ VendorApp/views.py
@@ -4,6 +4,7 @@ from rest_framework import status
 from .models import Vendor
 from .serializers import VendorSerializer
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
 # View vendors api/vendors/.
 
@@ -21,7 +22,8 @@ class VendorsList(APIView):
 class VendorCreate(APIView):
     @swagger_auto_schema(
         request_body=VendorSerializer,
-        responses={201: VendorSerializer()},
+        responses={201: VendorSerializer(),
+                   400: "Bad Request",},
     )
     def post(self, request):
         serializer = VendorSerializer(data=request.data)
@@ -29,3 +31,48 @@ class VendorCreate(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class VendorDetails(APIView):
+    @swagger_auto_schema(
+        responses={200: VendorSerializer},
+    )
+    def get(self, request, vendor_id):
+        vendor = Vendor.objects.get(id=vendor_id)
+        serializer = VendorSerializer(vendor)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+
+class VendorUpdate(APIView):
+    @swagger_auto_schema(
+        request_body=VendorSerializer(),
+        responses={
+            200: VendorSerializer(),
+            400: "Bad Request",
+            404: "Vendor Not Found"
+        },
+    )
+    def post(self, request, vendor_id):
+        try:
+            vendor =  Vendor.objects.get(id=vendor_id)
+            serializer = VendorSerializer(vendor, data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_200_OK)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Vendor.DoesNotExist:
+            return Response({'error': 'Vendor not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+
+class VendorDelete(APIView):
+    @swagger_auto_schema(
+        responses={204: "Vendor deleted",
+                   404: "Vendor not found"},
+    )
+    def delete(self, request, vendor_id):
+        try:
+            vendor =  Vendor.objects.get(id=vendor_id)
+            vendor.delete()
+            return Response({'message': 'Vendor deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
+        except Vendor.DoesNotExist:
+            return Response({'error': 'Vendor not found'}, status=status.HTTP_404_NOT_FOUND)
